a42f0c958e75c29393807b1be722f556d6981f55
Terry Bennett <tb594s@outlook.com>
2017-04-21 17:04:19 -0500

    Updated Documentation, Added GiveFeedback button in inspector

---
+++ Editor/CustomHelpInspector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.PracticalAssets.Practical_Analytics;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PracticalDLL))]
+public class CustomHelpInspector : Editor
+{
+
+	public override void OnInspectorGUI()
+	{
+		GUILayout.Label("Have feedback?");
+		if (GUILayout.Button("Give Feedback"))
+			GiveFeedback();
+	}
+
+	void GiveFeedback()
+	{
+		var helpButton = new PracticalDLL();
+		helpButton.SendEmail();
+		//Application.OpenURL("http://practicalvr.com/");
+	}
+
+}
